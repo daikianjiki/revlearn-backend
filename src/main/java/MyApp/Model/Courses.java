6d199f4c2636d8845795e3ab7b1c964ab58910ba
RECamp88 <recamp88@gmail.com>
2023-04-14 16:06:17 -0500

    Created the Courses Model, Service, Repo, and Controller.

--- src/main/java/MyApp/Model/Courses.java
+++ src/main/java/MyApp/Model/Courses.java
@@ -1,4 +1,37 @@
 package MyApp.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Courses {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long courseId;
+    private Long educatorId;
+    private String name; // added field as there was no field for the actual name of the course
+    private String keywords;
+    private String category;
+    private String level;
+    private String description;
+    private Double cost;
+
 }
+
+/*In satisfying user stories for students to be able to register for a class, interact within a class, get their grades for a class
+there needed to be a model for a course.
+This model leverages the use of Spring ORM with JpaRepository for the creation of the table and handling of they SQL calls.
+It also leverages Lombok for creation of all setters, getters, and constructors.
+
+Relationships:
+1-to-many -> student < courses
+1-to-many -> educators < courses
+1-to-1 -> course to forum */
---
+++ src/main/java/MyApp/Repository/CourseRepo.java
@@ -0,0 +1,13 @@
+package MyApp.Repository;
+
+import MyApp.Model.Courses;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepo extends JpaRepository<Courses, Long> {
+}
+
+/*Leveraging the JpaRepository aids with SQL queries as it has many built in methods that
+have been utilized.
+ */
