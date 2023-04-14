4f2c12402eada0c40060c8de268be67f23ed6e36
Daiki Anjiki <daikianjiki@gmail.com>
2023-04-14 12:41:22 -0700

    basic functionalities of Quiz completed

---
+++ src/main/java/MyApp/Controller/QuizController.java
@@ -0,0 +1,33 @@
+package MyApp.Controller;
+
+import MyApp.Model.Quiz;
+import MyApp.Service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = {"*"})
+@RestController
+public class QuizController {
+    QuizService quizService;
+    @Autowired
+    QuizController(QuizService quizService) {
+        this.quizService = quizService;
+    }
+    @GetMapping("quiz")
+    public ResponseEntity<List<Quiz>> getAllQuizzes() {
+        return this.quizService.getAllQuizzes();
+    }
+    @GetMapping("quiz/{id}")
+    public ResponseEntity<?> getQuizById(@PathVariable long id) {
+        return this.quizService.getQuizById(id);
+    }
+    @PostMapping("quiz")
+public ResponseEntity<?> createQuiz(@RequestBody Quiz quiz) {
+        return this.quizService.createQuiz(quiz);
+    }
+
+}
--- src/main/java/MyApp/Model/Quiz.java
+++ src/main/java/MyApp/Model/Quiz.java
@@ -1,4 +1,24 @@
 package MyApp.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long quizId;
+    public String quizName;
+    public String answer1;
+    public String answer2;
+    public String answer3;
+    public String answer4;
 }
