44a93160426add0a765896c87d8ff41b0d78e06b
Angel Torres <angelisraeltorres@outlook.com>
2023-04-17 08:36:03 -0700

    post register and student

--- src/main/java/MyApp/Model/Login.java
+++ src/main/java/MyApp/Model/Login.java
@@ -1,4 +1,33 @@
 package MyApp.Model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Login {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String email;
+    @Column
+    private String password;
+    @Column
+    private String user_type;
+    @Column
+    private String status;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    @JoinColumn(name = "loginFK")
+    private Student student;
+
 }
