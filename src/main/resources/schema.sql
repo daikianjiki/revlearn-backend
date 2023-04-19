9bebf51c137c3842c496658c520e14ca6c6184f9
RECamp88 <recamp88@gmail.com>
2023-04-19 16:04:26 -0500

    updating schema file for the junciton table for courses and students

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -47,3 +47,11 @@ CREATE TABLE forum (
   text_body LONGTEXT,
   created_at TIMESTAMP
 );
+
+CREATE TABLE course_student (
+  course_id INT NOT NULL,
+  student_id INT NOT NULL,
+  PRIMARY KEY (course_id, student_id),
+  FOREIGN KEY (course_id) REFERENCES courses(course_id),
+  FOREIGN KEY (student_id) REFERENCES student(id)
+);
