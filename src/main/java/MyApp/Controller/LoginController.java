997048b5100deed77fb0a9f5c5ed44a87370c6aa
Angel Torres <angelisraeltorres@outlook.com>
2023-04-19 14:50:31 -0700

    duplicate user exception

--- src/main/java/MyApp/Controller/LoginController.java
+++ src/main/java/MyApp/Controller/LoginController.java
@@ -1,5 +1,6 @@
 package MyApp.Controller;
 
+import MyApp.Exceptions.DuplicateUserException;
 import MyApp.Exceptions.UnauthorizedUserException;
 import MyApp.Model.Login;
 import MyApp.Service.LoginService;
@@ -24,7 +25,7 @@ public class LoginController {
      * POST localhost:9000/register
      */
     @PostMapping("register")
-    public Login register(@RequestBody Login login) {
+    public Login register(@RequestBody Login login) throws DuplicateUserException {
         return loginService.register(login);
     }
 
@@ -50,6 +51,11 @@ public class LoginController {
     @ExceptionHandler(UnauthorizedUserException.class)
     @ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "invalid login credentials, please try again!")
     public void handleUnauthorized(){
+    }
+
+    @ExceptionHandler(DuplicateUserException.class)
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "This user is already registered, please try again!")
+    public void handleDuplicate(){
     }
 
     /**
